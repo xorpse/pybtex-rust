e02144d2f4ca4175ccca1de082e88ae17e82ca80
Sam Thomas <m@kali.ai>
2018-06-27 20:54:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pybtex-rust
+
+Rust interface to obtain bibliography entries using pybtex via Python FFI.
---
+++ src/lib.rs
@@ -0,0 +1,94 @@
+//! Simple interface to pybtex to extract bibliography entries.
+
+extern crate cpython;
+
+use cpython::{Python, NoArgs, ObjectProtocol, PyErr};
+
+/// Simple bibliography entry with non-standard fields.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Entry {
+    pub key: String,
+    pub title: String,
+    pub authors: Option<Vec<String>>,
+    pub booktitle: Option<String>,
+    pub series: Option<String>,
+    pub note: Option<String>,
+    pub slides: Option<String>,
+    pub pdf: Option<String>,
+    pub year: u16,
+}
+
+/// Parses a bibtex bibliography via pybtex and extracts an `Entry` for each bibliography entry.
+pub fn parse_bibliography(file: &str) -> Result<Vec<Entry>, PyErr> {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let db = py.import("pybtex.database")?;
+    let plain = py.import("pybtex.style.names.plain")?.call(py, "NameStyle", NoArgs, None)?;
+    let bib_data = db.call(py, "parse_file", (file,), None)?;
+
+    let entries = bib_data
+        .getattr(py, "entries")?
+        .call_method(py, "values", NoArgs, None)?
+        .iter(py)?;
+
+    let mut bib_entries = Vec::new();
+
+    for entry in entries {
+        let entry = entry.unwrap();
+        let key: String = entry.getattr(py, "key")?.extract(py)?;
+        let rich_fields = entry.getattr(py, "rich_fields")?;
+
+        let year: u16 = if let Ok(year) = rich_fields.get_item(py, "year").and_then(|v| v.str(py)).and_then(|v| v.to_string(py).map(String::from)) {
+            year.parse::<u16>().unwrap()
+        } else {
+            continue
+        };
+
+        let title = if let Ok(title) = rich_fields.get_item(py, "title").and_then(|v| v.str(py)).and_then(|v| v.to_string(py).map(String::from)) {
+            title
+        } else {
+            continue
+        };
+
+        let authors = if let Ok(authors) = entry.getattr(py, "persons")?.get_item(py, "author") {
+        // In [73]: for ent in bib_data.entries.values():
+        //     ...:     for author in ent.persons['author']:
+        //     ...:         print(NameStyle().format(author).format().render_as('plaintext'))
+            let mut author_names = Vec::new();
+            for author in authors.iter(py)? {
+                let author = plain.call_method(py, "format", (author?,), None)?
+                    .call_method(py, "format", NoArgs, None)?
+                    .call_method(py, "render_as", ("plaintext",), None)?
+                    .str(py)?
+                    .to_string(py)?
+                    .into_owned();
+                author_names.push(author);
+            };
+            Some(author_names)
+        } else {
+            None
+        };
+
+        let booktitle = rich_fields.get_item(py, "booktitle").and_then(|v| v.str(py)).and_then(|v| v.to_string(py).map(String::from)).ok();
+
+        let series = rich_fields.get_item(py, "series").and_then(|v| v.str(py)).and_then(|v| v.to_string(py).map(String::from)).ok();
+
+        let note = rich_fields.get_item(py, "note").and_then(|v| v.str(py)).and_then(|v| v.to_string(py).map(String::from)).ok();
+        let slides = rich_fields.get_item(py, "_slides").and_then(|v| v.str(py)).and_then(|v| v.to_string(py).map(String::from)).ok();
+        let pdf = rich_fields.get_item(py, "_pdf").and_then(|v| v.str(py)).and_then(|v| v.to_string(py).map(String::from)).ok();
+
+        bib_entries.push(Entry {
+            key,
+            title,
+            authors,
+            booktitle,
+            series,
+            note,
+            slides,
+            pdf,
+            year,
+        });
+    }
+
+    Ok(bib_entries)
+}
